15739b60c7c77f585afd8eb84e0f81880b750428
Nurdaulet002 <73096695+Nurdaulet002@users.noreply.github.com>
2023-09-13 23:35:22 +0600

    wef

--- customers/views.py
+++ customers/views.py
@@ -59,10 +59,27 @@ class CustomerListView(TemplateResponseMixin, View):
 
 class ExportCustomersView(View):
 
+    def get_date_range(self, range_type):
+        today = datetime.now()
+        if range_type == "all-time":
+            earliest_date = Customer.objects.all().order_by('created_at').first().created_at.date()
+            return earliest_date, today
+        start_date_methods = {
+            "week": lambda: today - timedelta(days=6),
+            "month": lambda: today.replace(day=1),
+            "quarter": lambda: today.replace(month=((today.month - 1) // 3) * 3 + 1, day=1),
+            "half-year": lambda: today.replace(month=1, day=1) if today.month > 6 else today.replace(
+                year=today.year - 1, month=7, day=1),
+            "year": lambda: today.replace(month=1, day=1)
+        }
+        start_date = start_date_methods.get(range_type, lambda: None)()
+        return start_date, today
+
     def get(self, request, *args, **kwargs):
         # Get filter criteria from request
         branch_id = request.GET.get('branch')
         range_type = request.GET.get('range_type')
+
 
         # Apply filters to fetch customers
         customers = self.get_filtered_customers(branch_id, range_type)
@@ -75,32 +92,14 @@ class ExportCustomersView(View):
         return response
 
     def get_filtered_customers(self, branch_id, range_type):
-        # Build your filter criteria based on branch_id and range_type
-        filters = {}
+        start_date, end_date = self.get_date_range(range_type)
+        filters = {
+            'created_at__range': (start_date, end_date),
+        }
 
         if branch_id != 'all':
             filters['branch__id'] = branch_id
 
-        if range_type == 'week':
-            # Calculate date range for the past week and add it to filters
-            end_date = datetime.now()
-            start_date = end_date - timedelta(days=7)
-            filters['created_at__range'] = (start_date, end_date)
-        elif range_type == 'month':
-            # Calculate date range for the past month and add it to filters
-            end_date = datetime.now()
-            start_date = end_date - timedelta(days=30)
-            filters['created_at__range'] = (start_date, end_date)
-        elif range_type == 'half-year':
-            # Calculate date range for the past six months and add it to filters
-            end_date = datetime.now()
-            start_date = end_date - timedelta(days=180)
-            filters['created_at__range'] = (start_date, end_date)
-        elif range_type == 'year':
-            # Calculate date range for the past year and add it to filters
-            end_date = datetime.now()
-            start_date = end_date - timedelta(days=365)
-            filters['created_at__range'] = (start_date, end_date)
 
         # Fetch filtered customers
         customers = Customer.objects.filter(**filters)
